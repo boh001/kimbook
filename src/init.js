909882ebc9e2e5eb633c3b7cb3de25630f5f5906
boh001 <boh001g@gmail.com>
2020-02-02 02:16:03 +0900

    prestart

--- src/init.js
+++ src/init.js
@@ -4,6 +4,10 @@ dotenv.config();
 import app from "./app";
 import "./db";
 import "./models/User";
+import "./models/Message";
+import "./models/Content";
+import "./models/Comment";
+import "./models/ChatRoom";
 import socketIO from "socket.io";
 import SocketController from "./controllers/socketCon";
 
