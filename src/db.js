7e9a4e4dd742cce241214580067361800791c0cf
boh001 <boh001g@gmail.com>
2020-02-03 13:32:16 +0900

    2020-02-03

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mongoose.connection;
-const MONGO_URL = process.env.Production
+const MONGO_URL = process.env.PRODUCTION
   ? process.env.MONGOATLAS
   : process.env.MONGO_URL;
 db.on("error", () => console.log("DB connection Error!!"));
